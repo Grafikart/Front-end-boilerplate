22408071c585610699a196c45ec03fb0e5aace1e
Grafikart <contact@grafikart.fr>
2016-04-02 00:51:37 +0200

    Fix dev-server

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,19 +1,28 @@
 var webpack = require('webpack')
 var config = require('./webpack.dev.conf.js')
 var WebpackDevServer = require('webpack-dev-server')
-var port = 8080
+var port = 3003
+Object.keys(config.entry).forEach(function (name) {
+  config.entry[name] = ['webpack-dev-server/client?http://localhost:' + port + '/', 'webpack/hot/dev-server'].concat(config.entry[name])
+})
 var compiler = webpack(config)
+
 
 var server = new WebpackDevServer(compiler, {
   contentBase: "./",
   hot: true,
   historyApiFallback: false,
+  /*
   proxy: {
-    // "*": "http://isitweekendyet.com"
+    "*": {
+      target: "http://monsite.fr/...",
+      changeOrigin: true
+    }
   },
+  */
   quiet: false,
   noInfo: false,
-  publicPath: "/dist/",
+  publicPath: config.output.publicPath,
   stats: { colors: true }
 })
 
--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,10 +2,6 @@ var webpack = require('webpack')
 var merge = require('webpack-merge')
 var config = require('./webpack.base.conf')
 
-Object.keys(config.entry).forEach(function (name) {
-  config.entry[name] = ['webpack-dev-server/client?http://localhost:8080/', 'webpack/hot/dev-server'].concat(config.entry[name])
-})
-
 config.module.loaders.push({
   test: /\.scss$/,
   loaders: ['style', 'css', 'sass']
