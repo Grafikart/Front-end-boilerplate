bc76429a84b26cc62eefc83482bd80f5199c2169
Grafikart <contact@grafikart.fr>
2016-04-02 02:55:48 +0200

    Ajout sourcemap

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,13 +2,15 @@ var webpack = require('webpack')
 var merge = require('webpack-merge')
 var config = require('./webpack.base.conf')
 
+config.devtool = '#eval-source-map'
 config.module.loaders.push({
   test: /\.scss$/,
   loaders: ['style', 'css', 'sass']
 })
 
-config.plugins.unshift(
-  new webpack.HotModuleReplacementPlugin()
-)
+config.plugins = [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+].concat(config.plugins)
 
 module.exports = config
