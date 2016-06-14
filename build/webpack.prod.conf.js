57cd916677a6cc9768d5cc4ab053ca3fcda9339c
Alaa El Helw <sicksadsun@gmail.com>
2016-06-14 17:26:18 +0200

    Changed to match the Correct index of css loaders

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -18,9 +18,10 @@ config.plugins = config.plugins.concat([
 // Fournir la liste des index des loaders
 var css = [0, 1]
 for(k in css){
-  var cssLoaders = config.module.loaders[css[k]].loaders
-  config.module.loaders[k].loaders = null
-  config.module.loaders[k].loader = extractSASS.extract(cssLoaders.slice(1, 10))
+  var idx = css[k]
+  var cssLoaders = config.module.loaders[idx].loaders
+  config.module.loaders[idx].loaders = null
+  config.module.loaders[idx].loader = extractSASS.extract(cssLoaders.slice(1, 10))
 }
 
 module.exports = config;
